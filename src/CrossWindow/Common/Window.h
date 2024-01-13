ca477e14560f5bcabd73d5a5538599387c685679
Vincenzo Barbato <navyenzo@gmail.com>
2024-01-13 02:13:49 -0800

    fixed dangling elif statements without the "defined" keyword

--- src/CrossWindow/Common/Window.h
+++ src/CrossWindow/Common/Window.h
@@ -2,21 +2,21 @@
 
 #ifdef XWIN_WIN32
 #include "../Win32/Win32Window.h"
-#elif XWIN_UWP
+#elif defined(XWIN_UWP)
 #include "../UWP/UWPWindow.h"
-#elif XWIN_COCOA
+#elif defined(XWIN_COCOA)
 #include "../Cocoa/CocoaWindow.h"
-#elif XWIN_XCB
+#elif defined(XWIN_XCB)
 #include "../XCB/XCBWindow.h"
-#elif XWIN_XLIB
+#elif defined(XWIN_XLIB)
 #include "../XLib/XLibWindow.h"
-#elif XWIN_ANDROID
+#elif defined(XWIN_ANDROID)
 #include "../Android/AndroidWindow.h"
-#elif XWIN_UIKIT
+#elif defined(XWIN_UIKIT)
 #include "../UIKit/UIKitWindow.h"
-#elif XWIN_WASM
+#elif defined(XWIN_WASM)
 #include "../WASM/WASMWindow.h"
-#elif XWIN_NOOP
+#elif defined(XWIN_NOOP)
 #include "../Noop/NoopWindow.h"
 #endif
 
@@ -27,3 +27,4 @@ namespace xwin
 typedef std::shared_ptr<Window> WindowPtr;
 typedef std::weak_ptr<Window> WindowWeakPtr;
 }
+
