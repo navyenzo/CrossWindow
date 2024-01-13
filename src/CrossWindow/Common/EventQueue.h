a5e385cd13f7c35bd18cbf9e975b34c4c8328d96
Vincenzo Barbato <navyenzo@gmail.com>
2024-01-13 02:19:36 -0800

    fixed more dangling elif statements with missing "deifned" keyword

--- src/CrossWindow/Common/EventQueue.h
+++ src/CrossWindow/Common/EventQueue.h
@@ -6,20 +6,20 @@
 
 #ifdef XWIN_WIN32
 #include "../Win32/Win32EventQueue.h"
-#elif XWIN_UWP
+#elif defined(XWIN_UWP)
 #include "../UWP/UWPEventQueue.h"
-#elif XWIN_COCOA
+#elif defined(XWIN_COCOA)
 #include "../Cocoa/CocoaEventQueue.h"
-#elif XWIN_XCB
+#elif defined(XWIN_XCB)
 #include "../XCB/XCBEventQueue.h"
-#elif XWIN_XLIB
+#elif defined(XWIN_XLIB)
 #include "../XLib/XLibEventQueue.h"
-#elif XWIN_ANDROID
+#elif defined(XWIN_ANDROID)
 #include "../Android/AndroidEventQueue.h"
-#elif XWIN_UIKIT
+#elif defined(XWIN_UIKIT)
 #include "../UIKit/UIKitEventQueue.h"
-#elif XWIN_WASM
+#elif defined(XWIN_WASM)
 #include "../WASM/WASMEventQueue.h"
-#elif XWIN_NOOP
+#elif defined(XWIN_NOOP)
 #include "../Noop/NoopEventQueue.h"
 #endif
